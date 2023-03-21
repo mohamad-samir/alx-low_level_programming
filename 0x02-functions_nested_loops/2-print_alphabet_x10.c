d7a3afbade3c461c2e1b8dcb448dd36f99454c15
mohamed <mohamad.samir2011@gmail.com>
2023-03-21 16:23:29 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
+ * Description: print_alphabet
+ * - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Succes)
  */
