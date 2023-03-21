fb8f521d1ca87722961d2d83a087690617c67d7c
mohamed <mohamad.samir2011@gmail.com>
2023-03-21 16:54:32 -0700

    update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _isalpha(int c) a function that checks for alphabetic character.
+  * @c is a letter, lowercase or uppercase
+  * Returns 1 if c is a letter, lowercase or uppercase
+  * Returns 0 otherwise
+  */
+
+int main(void)
+{
+	if (c >= 'a' && c <= 'z') || (c <= 'A' && c >= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
