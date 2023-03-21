238a7cff478d1da05fbb5f04611897d53e3535e6
mohamed <mohamad.samir2011@gmail.com>
2023-03-21 15:44:20 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	while(i == 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return;
+}
