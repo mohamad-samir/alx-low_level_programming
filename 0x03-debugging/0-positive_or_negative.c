eae2997be0e77ff6e257a8d097138e5de11c2ce2
mohamed <mohamad.samir2011@gmail.com>
2023-03-22 14:52:05 -0700

    update

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void positive_or_nigative(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		 printf("%d is zero\n", n);
+	}
+}
