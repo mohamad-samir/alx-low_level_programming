07e15d5fdb19d259b8c2386db567d7eb1b510d02
mohamed <mohamad.samir2011@gmail.com>
2023-03-27 19:08:35 -0700

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,16 @@
 /**
  * _strlen - Write a function that returns the length of a string.
  * @s: a null-terminated string input.
+ * Return: the length of the string.
  */
 int _strlen(char *s)
 {
 	int len = 0;
+
 	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-	return len;
+	return (len);
 }
