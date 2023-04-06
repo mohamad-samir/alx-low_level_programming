0e9e2e111bcc92ba6495f97262d5bcd0f5d0829b
mohamed <mohamad.samir2011@gmail.com>
2023-04-05 19:47:29 -0700

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,37 @@
 #include "holberton.h"
 /**
- * wildcmp - compares two string
- * @s1: string1
- * @s2: string2
- * Return: 1 if equal 0 otherwise
+ * is_palindrome - is palindrome
+ * @s: string
+ * Return: 1 if palindrome, 0 otherwise
  */
-int wildcmp(char *s1, char *s2)
+int is_palindrome(char *s)
 {
-	if (*s1 != *s2)
+	return (pali(s, s + _strlen_recursion(s) - 1));
+}
+/**
+ * pali - is palindrome
+ * @s: start of string
+ * @e: end of string
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int pali(char *s, char *e)
+{
+	if (*s != *e)
 		return (0);
-	else if (*s1 == 0 && *s2 == 0)
+	else if (s >= e)
 		return (1);
-	else if (*s2 == '*')
-		wildcmp(s1 + 1, s2);
-	else if (*s2 == '*' && *s1 == *(s2 + 1))
-		return (wildcmp(s1 +1, s2 + 1));
-	return (wildcmp(s1 + 1, s2 + 1));
+	else
+		return (pali(s + 1, e - 1));
+}
 
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: string
+ * Return: length of str
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
