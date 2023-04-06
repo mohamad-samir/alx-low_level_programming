4bc4adee4e5e59c854005b3326309bc511a8948a
mohamed <mohamad.samir2011@gmail.com>
2023-04-05 19:53:56 -0700

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,37 +1,71 @@
+
+ 
+
 #include "main.h"
+
 /**
- * is_palindrome - is palindrome
- * @s: string
- * Return: 1 if palindrome, 0 otherwise
+ * move_past_star - iterates past asterisk
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: the pointer past star
  */
-int is_palindrome(char *s)
+char *move_past_star(char *s2)
 {
-	return (check_pal(s, s + _strlen_recursion(s) - 1));
-}
-/**
- * pali - is palindrome
- * @s: start of string
- * @e: end of string
- * Return: 1 if palindrome, 0 otherwise
- */
-int pali(char *s, char *e)
-{
-	if (*s != *e)
-		return (0);
-	else if (s >= e)
-		return (1);
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
 	else
-		return (check_pal(s + 1, e - 1));
+		return (s2);
 }
 
 /**
- * _strlen_recursion - return the length of a string
- * @s: string
- * Return: length of str
+ * inception - makes magic a reality
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: 1 if identical, 0 if false
  */
-int _strlen_recursion(char *s)
+int inception(char *s1, char *s2)
 {
-	if (*s == 0)
+	int ret = 0;
+
+	if (*s1 == 0)
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wildcmp - compares two strings lexicographically
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: 1 if identical, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+	return (0);
 }
