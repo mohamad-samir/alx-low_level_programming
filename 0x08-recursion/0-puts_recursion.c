1ed3d264310c4fd2fcd8a75e88ee62fe686c315c
mohamed <mohamad.samir2011@gmail.com>
2023-04-05 20:09:07 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to string
+ * *Return: void
  */
-void _puts_recursion(char *s)
+
+void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s > '\0')
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
-
-	else
-		_putchar('\n');
 }
-
