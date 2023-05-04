6618f3c1ef027ca5820dd44e4c858687a60d2fe6
Mohamed Samir <mohamedsamir@192.168.1.6>
2023-05-05 01:52:29 +0300

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,11 +4,12 @@
  * print_binary - prints the binary representation of a number
  * @n: the number to print
  */
+
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-        print_binary(n >> 1);
+	if (n > 1)
+	print_binary(n >> 1);
 
-    _putchar((n & 1) + '0');
+	_putchar((n & 1) + '0');
 }
 
