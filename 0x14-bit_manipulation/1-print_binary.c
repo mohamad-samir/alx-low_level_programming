a1745d85c0d2f0876cb97c64204d033e0f651338
Mohamed Samir <mohamedsamir@192.168.1.6>
2023-05-05 01:49:36 +0300

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,27 +2,13 @@
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: the number to print in binary form
- *
- * Return: void
+ * @n: the number to print
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-	int i;
+    if (n > 1)
+        print_binary(n >> 1);
 
-	/* Find the highest bit position that is set */
-	while ((mask << 1) <= n)
-		mask <<= 1;
-
-	/* Print the binary digits from left to right */
-	if (n == 0)
-		_putchar('0');
-	for (i = 0; mask > 0; mask >>= 1, i++)
-	{
-		if ((n & mask) == mask)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+    _putchar((n & 1) + '0');
 }
+
