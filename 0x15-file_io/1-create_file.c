c654c7c0c99b76e1ff7da210d52c7b3ef174e951
Mohamed Samir <mohamedsamir@192.168.1.6>
2023-05-09 01:53:20 +0300

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,41 @@
 #include "main.h"
+
 /**
-  * create_file - creates a file
-  * @filename: name of the file
-  * @text_content: content to be in the file
-  * Return: 1 if successful, -1 if it fails
-  */
+ * create_file - creates a file with the given filename and writes
+ *				 the given text content to it
+ *
+ * @filename: the name of the file to create
+ * @text_content: a NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, nwr;
+	int fd, num_written, text_len = 0;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (text_content)
+	{
+		while (text_content[text_len])
+			text_len++;
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
-		text_content = "";
 
-	for (len = 0; text_content[len]; len++)
-		;
-	nwr = write(fd, text_content, len);
-
-	if (nwr == -1 || nwr != len)
+	if (text_len > 0)
 	{
-		close(fd);
-		return (-1);
+		num_written = write(fd, text_content, text_len);
+		if (num_written != text_len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
+
 	close(fd);
 	return (1);
 }
