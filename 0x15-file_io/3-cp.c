5127d4cf23c8cc97eaf89e75ee45697bce15b086
Mohamed Samir <mohamedsamir@192.168.1.6>
2023-05-09 02:14:31 +0300

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,10 @@
 
 #define BUFSIZE 1024
 
+void error_exit(char *message, int exit_code);
+void copy_file(int fd_from, int fd_to);
+void close_fd(int fd);
+
 /**
  * main - copies the content of a file to another file.
  * @argc: number of arguments.
@@ -14,33 +18,67 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, rd, wr;
-	char buf[BUFSIZE];
+	int fd_from, fd_to;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		error_exit("Error: Can't read from file %s\n", 98);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		error_exit("Error: Can't write to %s\n", 99);
+
+	copy_file(fd_from, fd_to);
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
+
+/**
+ * error_exit - prints an error message to stderr and exits with an exit code.
+ * @message: error message.
+ * @exit_code: exit code.
+ */
+void error_exit(char *message, int exit_code)
+{
+	dprintf(STDERR_FILENO, message, argv[1]);
+	exit(exit_code);
+}
+
+/**
+ * copy_file - copies the content of a file to another file.
+ * @fd_from: file descriptor of the file to copy from.
+ * @fd_to: file descriptor of the file to copy to.
+ */
+void copy_file(int fd_from, int fd_to)
+{
+	int rd, wr;
+	char buf[BUFSIZE];
 
 	do {
 		rd = read(fd_from, buf, BUFSIZE);
 		if (rd == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			error_exit("Error: Can't read from file %s\n", 98);
 		wr = write(fd_to, buf, rd);
 		if (wr == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+			error_exit("Error: Can't write to %s\n", 99);
 	} while (rd == BUFSIZE);
+}
 
-	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
-	return (0);
+/**
+ * close_fd - closes a file descriptor and exits with an exit code
+ * if an error occurs.
+ * @fd: file descriptor to close.
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+		error_exit("Error: Can't close fd %d\n", 100);
 }
