9dbd348dc237670477c8e4279baa43a6f0edc557
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-15 21:45:58 +0300

    update

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -22,15 +22,16 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return (new_node);
 	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
 
-	temp = *head;
-	while (temp->next != NULL)
-		temp = temp->next;
-
-	temp->next = new_node;
-	new_node->prev = temp;
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
 
 	return (new_node);
 }
