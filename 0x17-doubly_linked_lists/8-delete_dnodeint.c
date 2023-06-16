41a5abe7dec7b0073955d6cc404059ac61d7d20f
mohamad-samir <mohamad.samir2011@gmail.com>
2023-06-16 17:34:32 +0300

    update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,50 +9,35 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int i;
-
-	// Check if the head or the list is empty
 	if (head == NULL || *head == NULL)
 		return (-1);
-		
-	// Set the current pointer to the head of the list
-	current = *head;
-	
-	// If the index is 0 (deleting the head node)
+
+	dlistint_t *nodeToDelete = *head;
+
 	if (index == 0)
 	{
-		// Update the head to the next node
-		*head = current->next;
-		
-		// Update the previous pointer of the new head, if it exists
-		if (current->next != NULL)
-		{
-			current->next->prev = NULL;
-		}
-		
-		// Free the memory of the deleted node
-		free(current);
+		*head = (*head)->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(nodeToDelete);
 		return (1);
 	}
-	
-	// Traverse the list to the node at the specified index
-	for (i = 0; i < index; i++)
+
+	for (unsigned int i = 0; i < index; i++)
 	{
-		// If we reach the end of the list before the desired index, return -1 indicating failure
-		if (current->next == NULL)
+		if (nodeToDelete == NULL)
 			return (-1);
-		
-		// Move to the next node
-		current = current->next;
+		nodeToDelete = nodeToDelete->next;
 	}
-	
-	// Update the next and previous pointers of the surrounding nodes to skip the node at the specified index
-	current->prev->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-		
-	// Free the memory of the deleted node
-	free(current);
+
+	if (nodeToDelete == NULL)
+		return (-1);
+
+	if (nodeToDelete->prev)
+		nodeToDelete->prev->next = nodeToDelete->next;
+	if (nodeToDelete->next)
+		nodeToDelete->next->prev = nodeToDelete->prev;
+
+	free(nodeToDelete);
 	return (1);
 }
