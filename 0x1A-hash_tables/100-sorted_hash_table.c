f40661ad05647b7905243f34fd18791034054317
mohamad-samir <mohamad.samir2011@gmail.com>
2023-07-08 20:30:12 +0300

    update

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -230,26 +230,24 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
  */
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	shash_node_t *node;
+    unsigned long int index;
+    shash_node_t *node;
 
-	if (ht == NULL)
-		return (NULL);
-	if (key == NULL)
-		return (NULL);
-	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
-		return (NULL);
-	if (strcmp(ht->array[index]->key, key) == 0)
-		return (ht->array[index]->value);
-	node = ht->array[index];
-	while (node != NULL)
-	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
-	}
-	return (NULL);
+    if (ht == NULL || key == NULL)
+        return (NULL);
+
+    index = key_index((unsigned char *)key, ht->size);
+    node = ht->array[index];
+
+    while (node != NULL)
+    {
+        if (strcmp(node->key, key) == 0)
+            return (node->value);
+
+        node = node->next;
+    }
+
+    return (NULL);
 }
 
 /**
